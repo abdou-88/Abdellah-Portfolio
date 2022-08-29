bc325b927a75e9b20fb43e8e333c858381bc464b
Abdellah Youssfi <abdellah.y.ouahabi@gmail.com>
2022-08-29 19:35:20 -0400

    aboutme

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -4,6 +4,6 @@ export const aboutData = {
   description1:
     "Hello! My name is Abdellah Yousfi. I'm an aspiring Web and Sharepoint developer based in Poland.",
   description2:
-    "I enjoy turning complex problems into a simple and beautiful solutions. Currently, I work at Lumen Technologies as a SharePoint developer, I have designed and built SharePoint extensions and webparts for internal stakeholders When im not coding or automating a workflow, you will find me in the gym or freestyle wrestling with my kid ",
+    "I enjoy turning complex problems into a simple and beautiful solutions. Currently, I work at Lumen Technologies as a SharePoint developer, I have designed and built SharePoint extensions and webparts for internal stakeholders, with focus on fast, elegant and user experiences. When im not coding or automating a workflow, you will find me in the gym or freestyle wrestling with my kid ",
   image: 2,
 };
