c9079487e3fc98ff7083c87b0a74f7d7dcff88bb
Abdellah Youssfi <abdellah.y.ouahabi@gmail.com>
2022-08-30 17:01:25 -0400

    media queues added

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,7 +5,7 @@ import signature from "../assets/img/signature.png";
 
 export const headerData = {
   name: "Abdellah Yousfi",
-  title: "Office 365 Developer",
+  title: "Sharepoint Developer",
   desciption:
     "A highly motivated SharePoint Developer with hands-on experience of developing high quality Office 365 solutions and automations. Creative thinker with a solution-oriented approach.",
   image: image,
